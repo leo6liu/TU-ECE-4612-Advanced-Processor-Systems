8cc8b0d72e97b473cad99215d38dcb56eae31953
Leo <leo.battalora@gmail.com>
2021-11-03 19:50:00 -0700

    finished left shifter tests

--- SingleCycleProcessor/SingleCycleProcessor.srcs/sim_1/new/LeftShifter_tb.v
+++ SingleCycleProcessor/SingleCycleProcessor.srcs/sim_1/new/LeftShifter_tb.v
@@ -19,8 +19,8 @@ module LeftShifter_tb();
     LeftShifter M1(.out(out),.in(in));
     
     initial begin
-        #10 in = 32'b1111;     // 0b 1111 00
-        #10 in = 32'b0000;     // 0x 0000 00
+        #10 in = 32'b1111;     // 0x 003C
+        #10 in = 32'b0000;     // 0x 0000
         #10 $finish;
     end
 endmodule
